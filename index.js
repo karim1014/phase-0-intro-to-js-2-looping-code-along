921ec459eac20b2ed2e0cc031ea4fef8c5e2dfeb
karim rezk <bigkoko882@gmail.com>
2023-03-13 16:58:41 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+
+ 
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
